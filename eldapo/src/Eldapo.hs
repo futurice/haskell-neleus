b8e814f054961091c79f04dd5bafd5db408ad999
Oleg Grenrus <oleg.grenrus@iki.fi>
2017-12-15 14:18:00 +0200

    Schema encoding

--- eldapo/src/Eldapo.hs
+++ eldapo/src/Eldapo.hs
@@ -1,12 +1,23 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Eldapo where
 
-import Neleus
-import Network.Arcola (Connection, connLazyRead, connLazySendAll, run)
+import Prelude ()
+import Prelude.Compat
 
-import qualified Data.ByteString.Lazy as LBS
+import Data.Foldable      (for_)
+import Data.List.NonEmpty (NonEmpty (..))
+import Neleus
+import Network.Arcola     (Connection, connLazyRead, connLazySendAll, run)
+
 import qualified Data.ByteString.Base16.Lazy as LBS16
-import qualified Eldapo.Types as T
+import qualified Data.ByteString.Lazy        as LBS
+import qualified Data.ByteString.Lazy.Char8  as LBS8
+import qualified Data.Set                    as Set
+import qualified Eldapo.Types                as T
+
+import qualified Data.ByteString.Base16.Lazy as LBS16
 
 defaultMain :: IO ()
 defaultMain = do
@@ -20,12 +31,60 @@ defaultMain = do
   where
     loop :: Connection -> Integer -> LBS.ByteString -> IO ()
     loop conn i lbs
-        | LBS.null lbs = print "DONE"
+        | LBS.null lbs = putStrLn "DONE"
         | otherwise = case decode stepDER schema lbs of
             Left err        -> do
-                print "ERROR"
-                print err
+                putStrLn "ERROR"
+                putStrLn err
             Right (x, lbs') -> do
-                print (x :: T.LDAPMessage)
-                connLazySendAll conn (encode (searchResDone i))
+                ys <- interactLDAP i x
+                for_ ys $ \y -> do
+                    let e = encode schema y
+                    putStrLn $ "<<< " ++ LBS8.unpack (LBS16.encode e)
+                    connLazySendAll conn e
                 loop conn (succ i) lbs'
+
+interactLDAP :: Integer -> T.LDAPMessage -> IO (NonEmpty T.LDAPMessage)
+interactLDAP i x@(T.LDAPMessage _ op _) = do
+    print x
+    case op of
+        Z (I br) -> do
+            y <- handleBindRequest br
+            return $ return $ T.LDAPMessage i (S (Z (I y))) Nothing
+        S (S (S (Z (I sr)))) -> do
+            ys <- handleSearchRequest sr
+            return $ snoc (fmap wrapEntry ys) (searchResDone i)
+        -- rest: return something :)
+        _        -> return $ return $ searchResDone i
+  where
+    handleBindRequest :: T.BindRequest -> IO T.BindResponse
+    handleBindRequest _ = return $ T.BindResponse
+        { T.brCode             = T.RCSuccess
+        , T.brMatchedDN        = T.LDAPDN ""
+        , T.brErrorMessage     = T.LDAPString ""
+        , T.brReferral         = Nothing
+        , T.brServerSaslsCreds = Nothing
+        }
+
+    handleSearchRequest :: T.SearchRequest -> IO [T.SearchResultEntry]
+    handleSearchRequest _ = pure
+        [ T.SearchResultEntry (T.LDAPDN "dn=root")
+            [ T.PartialAttribute "cn" (Set.singleton "toor")
+            ]
+        ]
+
+    wrapEntry :: T.SearchResultEntry -> T.LDAPMessage
+    wrapEntry e = T.LDAPMessage i (S . S . S . S . Z . I $ e) Nothing
+
+snoc :: [a] -> a -> NonEmpty a
+snoc []       a = a :| []
+snoc (x : xs) a = x :| (xs ++ [a])
+
+searchResDone :: Integer -> T.LDAPMessage
+searchResDone i = T.LDAPMessage i (S . S . S . S . S . Z . I $ msg) Nothing where
+    msg = T.SearchResultDone
+        { T.srdCode         = T.RCSuccess
+        , T.srdMatchedDN    = T.LDAPDN ""
+        , T.srdErrorMessage = T.LDAPString ""
+        , T.srdReferral     = Nothing
+        }
--- neleus/src/Neleus/Encode.hs
+++ neleus/src/Neleus/Encode.hs
@@ -1,17 +1,28 @@
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs     #-}
 module Neleus.Encode where
 
 import Prelude ()
 import Prelude.Compat
 
 import Data.Bits      ((.|.))
+import Data.Foldable  (toList)
+import Data.List      (genericLength)
 import Data.Semigroup
 
 import qualified Data.ByteString             as BS
-import qualified Data.ByteString.Base16.Lazy as LBS16
 import qualified Data.ByteString.Builder     as B
 import qualified Data.ByteString.Lazy        as LBS
+import qualified Data.Fin                    as Fin
 
+import Neleus.Generics
+import Neleus.Integer
+import Neleus.Schema
 import Neleus.Types
+
+-------------------------------------------------------------------------------
+-- B
+-------------------------------------------------------------------------------
 
 -- | A wrapper around 'Builder' which tracks the length.
 data B = B !Word B.Builder
@@ -26,60 +37,135 @@ instance Monoid B where
     mempty = B 0 mempty
     mappend = (<>)
 
-base16encode :: ASN1Value -> LBS.ByteString
-base16encode = LBS16.encode . encode
+-- | Identifier and data
+data IB = IB !Identifier !B
 
-encode :: ASN1Value -> LBS.ByteString
-encode x = case asn1B x of
-    B _ b -> B.toLazyByteString b
+ibToB :: IB -> B
+ibToB (IB (Identifier cls pc tag) b) = identifierB cls pc tag <> b
 
 -------------------------------------------------------------------------------
---
+-- Encode
 -------------------------------------------------------------------------------
 
-asn1B :: ASN1Value -> B
+encode
+    :: Schema a        -- ^ schema
+    -> a               -- ^ value
+    -> LBS.ByteString  -- ^ result is a stream
+encode s x =  case encodeB s x of
+    B _ bldr -> B.toLazyByteString bldr
+
+encodeB :: Schema a -> a -> B
+encodeB s x = ibToB (encodeA s x)
+
+encodeA :: Schema a -> a -> IB
+encodeA SAny           x  = asn1B x
+encodeA SInteger       i  = IB
+    (Identifier UniversalC Primitive 0x02)
+    (finiteB (bLen b) <> b)
+  where
+    b = integerB i
+encodeA SOctetString   bs = IB
+    (Identifier UniversalC Primitive 0x04)
+    (finiteB l <> B l (B.byteString bs))
+  where
+    l = fromIntegral (BS.length bs)
+encodeA SBool b = IB
+    (Identifier UniversalC Primitive 0x01)
+    (finiteB 1 <> B 1 (B.word8 $ if b then 1 else 0))
+encodeA SNull _= IB
+    (Identifier UniversalC Primitive 0x05)
+    (finiteB 0)
+
+encodeA (SNamed _ i s) x  = encodeA s (isoFrom i x)
+encodeA (STagged Explicit cls tag s) x = IB
+    (Identifier cls Constructed tag)
+    (encodeB s x)
+encodeA (STagged Implicit cls tag s) x = case encodeA s x of
+    IB (Identifier _ pc _) b -> IB (Identifier cls pc tag) b
+
+encodeA (SSequenceOf s) xs = IB
+    (Identifier UniversalC Constructed 0x10)
+    (finiteB (bLen b) <> b)
+  where
+    b = foldMap (encodeB s) xs
+
+encodeA (SSetOf s) xs = IB
+    (Identifier UniversalC Constructed 0x11)
+    (finiteB (bLen b) <> b)
+  where
+    b = foldMap (encodeB s) xs
+
+encodeA (SEnumeration _) j = IB
+    (Identifier UniversalC Primitive 0x0A)
+    (integerB $ toInteger $ Fin.toNatural j)
+
+encodeA (SSequence fs) xs = IB
+    (Identifier UniversalC Constructed 0x10)
+    (finiteB (bLen b) <> b)
+  where
+    b = go fs xs
+
+    go :: NP FieldSchema xs -> NP I xs -> B
+    go Nil Nil = mempty
+    go (Req _ s   :* fs') (I x        :* xs') = encodeB s x <> go fs' xs'
+    go (Def _ _ s :* fs') (I x        :* xs') = encodeB s x <> go fs' xs'
+    go (Opt _ _   :* fs') (I Nothing  :* xs') = go fs' xs'
+    go (Opt _ s   :* fs') (I (Just x) :* xs') = encodeB s x <> go fs' xs'
+
+encodeA (SChoice os) xs = go os xs
+  where
+    go :: NP OptionSchema xs -> NS I xs -> IB
+    go Nil x = case x of {}
+    go (SOption _ s :* _)   (Z (I x)) = encodeA s x
+    go (_           :* os') (S xs')   = go os' xs'
+
+-------------------------------------------------------------------------------
+-- Value
+-------------------------------------------------------------------------------
+
+asn1B :: ASN1Value -> IB
 asn1B EOC =
-    identifierB UniversalC Primitive 0x00 <> finiteB 0
+    IB (Identifier UniversalC Primitive 0x00) (finiteB 0)
 asn1B (Int i) =
-    identifierB UniversalC Primitive 0x02 <> finiteB (bLen b) <> b
+    IB (Identifier UniversalC Primitive 0x02) (finiteB (bLen b) <> b)
   where
     b = integerB i
 asn1B (OctetString bs) =
-    identifierB UniversalC Primitive 0x04 <> finiteB l <> B l (B.byteString bs)
+    IB (Identifier UniversalC Primitive 0x04) (finiteB l <> B l (B.byteString bs))
   where
     l = fromIntegral (BS.length bs)
 asn1B (Enum i) =
-    identifierB UniversalC Primitive 0x0A <> finiteB (bLen b) <> b
+    IB (Identifier UniversalC Primitive 0x0A) (finiteB (bLen b) <> b)
   where
     b = integerB i
 asn1B (Sequence xs) =
-    identifierB UniversalC Constructed 0x10 <> finiteB (bLen b) <> b
+    IB (Identifier UniversalC Constructed 0x10) (finiteB (bLen b) <> b)
   where
-    b = foldMap asn1B xs
+    b = foldMap (ibToB . asn1B) xs
 asn1B (Application tag x) =
     nonUniversalB ApplicationC tag x
 asn1B x = error (show x)
 
-nonUniversalB :: Class -> Tag -> BS -> B
-nonUniversalB cls tag (BS bs) = identifierB cls Primitive tag
-    <> finiteB l
-    <> B l (B.byteString bs)
+nonUniversalB :: Class -> Tag -> BS -> IB
+nonUniversalB cls tag (BS bs) = IB
+    (Identifier cls Primitive tag)
+    (finiteB l <> B l (B.byteString bs))
   where
     l = fromIntegral (BS.length bs)
-nonUniversalB cls tag (Value xs) = identifierB cls Constructed tag
-    <> finiteB (bLen b)
-    <> b
+nonUniversalB cls tag (Value xs) = IB
+    (Identifier cls Constructed tag)
+    (finiteB (bLen b))
   where
-    b = foldMap asn1B xs
+    b = foldMap (ibToB . asn1B) xs
 
 -------------------------------------------------------------------------------
 -- Integer
 -------------------------------------------------------------------------------
 
 integerB :: Integer -> B
-integerB i
-    | -128 <= i && i <= 127 = B 1 (B.int8 (fromInteger i))
-    | otherwise = error "big integer"
+integerB i = B (genericLength bs) (foldMap B.word8 bs)
+  where
+    bs = toList (encodeInteger i)
 
 -------------------------------------------------------------------------------
 -- Identifier
@@ -107,27 +193,10 @@ identifierB cls pc tag =
 finiteB :: Word -> B
 finiteB n
     | n < 0x80  = B 1 (B.word8 (fromIntegral n))
-    | otherwise = error "lengthB"
+    | otherwise = error "lengthB" -- TODO!!!
 
 indefiniteB :: B
 indefiniteB = B 1 (B.word8 0x80)
 
 lengthB :: Length -> B
 lengthB = maybe indefiniteB finiteB
-
--------------------------------------------------------------------------------
--- test value
--------------------------------------------------------------------------------
-
-searchResDone :: Integer -> ASN1Value
-searchResDone i = Sequence
-    [ Int i
-    , Application 5 $ Value $
-        [ Enum 0 -- success
-        -- matchedDN LDAPDN
-        , OctetString mempty
-        -- errorMessage LDAPString
-        , OctetString mempty
-        -- referral [3] Referral OPTIONAL
-        ]
-    ]
